53dff6344269153b09889759be6fdb02ae0d0f4e
jihyun-jeon <jhplus13@naver.com>
2025-03-04 17:21:40 +0900

    feat: swiper 활용한 캐러셀 구현

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -2,7 +2,7 @@ import axiosInstance from './axios';
 import { MovieResponse } from '../types/Movie';
 import { useQuery } from '@tanstack/react-query';
 
-export const usePopularMovies = (queryParams: { language: string; page: number }) => {
+export const usePopularMovies = (queryParams: { language: string; page?: number }) => {
   return useQuery({
     queryFn: () => {
       return axiosInstance<MovieResponse>({
@@ -15,7 +15,20 @@ export const usePopularMovies = (queryParams: { language: string; page: number }
   });
 };
 
+export const useTrendingMovies = (queryParams: { language: string; page?: number }) => {
+  return useQuery({
+    queryFn: () => {
+      return axiosInstance<MovieResponse>({
+        url: '/trending/movie/day',
+        method: 'get',
+        params: queryParams,
+      }).then((res) => res.data);
+    },
+    queryKey: MoviesQuery.getMany('getTrending', queryParams),
+  });
+};
+
 export const MoviesQuery = {
   all: ['movies'],
-  getMany: (getType: string, queryParams?: any) => [...MoviesQuery.all, getType, queryParams],
+  getMany: (getCategory: string, queryParams?: any) => [...MoviesQuery.all, getCategory, JSON.stringify(queryParams)],
 };
