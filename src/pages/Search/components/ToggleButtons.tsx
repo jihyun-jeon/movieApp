1d75acdf1f902b48fa0da55df12a7dbfc4435780
jihyun-jeon <jhplus13@naver.com>
2025-03-06 14:12:19 +0900

    feat: 장르 필터 toggle ui 구현

---
+++ src/pages/Search/components/ToggleButtons.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useGetGenres } from '@/api/tmdb';
+import { Genre } from '@/types/Movie';
+import { TMDB_LANGUAGE_KR } from '@/contants';
+import { ToggleGroup, ToggleGroupItem } from '@/shadcn/components/ui/toggle-group';
+
+interface ToggleButtonsProps {
+  selectedGenres: string[];
+  setSelectedGenres: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const ToggleButtons: React.FC<ToggleButtonsProps> = ({ selectedGenres, setSelectedGenres }) => {
+  const genreIds = useGetGenres({ language: TMDB_LANGUAGE_KR });
+
+  const handleToggleChange = (newValues: string[]) => {
+    setSelectedGenres(newValues);
+  };
+
+  return (
+    <ToggleGroup
+      type="multiple"
+      value={selectedGenres}
+      onValueChange={handleToggleChange}
+      aria-label="Genre selection"
+      className="flex flex-wrap gap-2"
+    >
+      {genreIds.data?.genres.map((data: Genre) => {
+        return (
+          <ToggleGroupItem
+            value={`${data.id}`}
+            key={data.id}
+            className={`${
+              selectedGenres?.includes(`${data.id}`) ? 'bg-white border-white' : 'bg-black border-[#84868d]'
+            } flex-none mr-1 py-2 px-4 rounded-lg transition-colors duration-200 border text-[#84868d]`}
+          >
+            {data.name}
+          </ToggleGroupItem>
+        );
+      })}
+    </ToggleGroup>
+  );
+};
+
+export default ToggleButtons;
