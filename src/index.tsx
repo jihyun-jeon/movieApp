3d1acf842fd22feeb7e0015a4d211c69d194c437
jihyun-jeon <jhplus13@naver.com>
2025-03-09 18:25:14 +0900

    chore: react-query devtool 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from '@/App';
 import '@/styles/main.css';
 
@@ -14,5 +15,6 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={true} />
   </QueryClientProvider>,
 );
