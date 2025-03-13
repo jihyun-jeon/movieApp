b3bdf332e271086b1a113549541cfafe2d117390
jihyun-jeon <jhplus13@naver.com>
2025-03-13 18:54:34 +0900

    refac: webpack 개발,프로덕환경별 파일 구분

---
+++ webpack.prod.js
@@ -0,0 +1,28 @@
+// webpack.prod.js
+
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production', // 프로덕션 모드 설정
+  devtool: 'hidden-source-map',
+  plugins: [...[new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })]].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+});
