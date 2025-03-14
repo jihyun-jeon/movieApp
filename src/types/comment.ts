435640a39636e7cffba033cf0604c439acaeb4f6
jihyun-jeon <jhplus13@naver.com>
2025-03-14 15:58:03 +0900

    refac: 타입에 TsDoc 형태로 주석 추가

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -1,9 +1,21 @@
+/** 영화 리뷰 정보 */
 export interface Comment {
+  /** 영화 고유 ID */
   movie_id: number;
-  review: string;
+  /** 사용자 ID (작성자) */
   user_id: string;
+  /** 사용자 이름 */
   user_name: string;
+  /** 리뷰 내용 */
+  review: string;
+  /** 평점 (1~10점) */
   vote: number;
 }
 
-export type DeleteCommentParams = { movieId: number; userId: string };
+/** 영화 리뷰 삭제 시 필요한 파라미터 */
+export type DeleteCommentParams = {
+  /** 영화 고유 ID */
+  movieId: number;
+  /** 사용자 ID */
+  userId: string;
+};
