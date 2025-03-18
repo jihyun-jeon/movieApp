54357b028f95fb775fdec642437aa7f20cd77a26
jihyun-jeon <jhplus13@naver.com>
2025-03-18 18:21:57 +0900

    feat: 세션 유효시 로그인,회원가입 페이지 가드처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import Layout from '@/components/Layout';
 import Home from '@/pages/Home';
@@ -6,8 +7,13 @@ import NotFound from '@/pages/NotFound';
 import Search from '@/pages/Search';
 import Signup from '@/pages/Signup';
 import Login from '@/pages/Login';
-import { AuthContextProvider } from '@/context/AuthContext';
+import { AuthContextProvider, useAuth } from '@/context/AuthContext';
 import Favorite from '@/pages/Favorite';
+
+const GuardRouter = ({ children }: { children: React.ReactNode }) => {
+  const { session } = useAuth();
+  return session ? <Navigate to="/movie" replace /> : children;
+};
 
 const router = createBrowserRouter([
   {
@@ -19,8 +25,22 @@ const router = createBrowserRouter([
       { path: 'movie/:movieId', element: <Detail /> },
       { path: 'search', element: <Search /> },
       { path: 'favorite', element: <Favorite /> },
-      { path: 'login', element: <Login /> },
-      { path: 'signup', element: <Signup /> },
+      {
+        path: 'login',
+        element: (
+          <GuardRouter>
+            <Login />
+          </GuardRouter>
+        ),
+      },
+      {
+        path: 'signup',
+        element: (
+          <GuardRouter>
+            <Signup />
+          </GuardRouter>
+        ),
+      },
     ],
   },
   { path: '*', element: <NotFound /> },
@@ -29,7 +49,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <AuthContextProvider>
-      <RouterProvider router={router} />;
+      <RouterProvider router={router} />
     </AuthContextProvider>
   );
 }
