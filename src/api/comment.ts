eeb18f350cae05fb7fe7c61c1e5386457f42e9bb
jihyun-jeon <jhplus13@naver.com>
2025-03-23 00:54:45 +0900

    refac: supabase 응답 처리 방식 통일 : useQuery의 queryFn은 Promise를 반환해야 하는데 supabase는 PromiseLike를 반환하는 문제 : Promise를 반환하도록 타입 강제처리

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -4,12 +4,12 @@ import { withSupabaseHandler } from '@/api/utils';
 
 /**  영화 댓글 조회  */
 export const fetchComments = (movieId: number) =>
-  supabase.from('Comments').select('*').eq('movie_id', movieId).then(withSupabaseHandler);
+  withSupabaseHandler(supabase.from('Comments').select('*').eq('movie_id', movieId));
 
 /** 영화 댓글 추가 */
 export const addComment = (commentData: Comment) =>
-  supabase.from('Comments').insert([commentData]).then(withSupabaseHandler);
+  withSupabaseHandler(supabase.from('Comments').insert([commentData]));
 
 /**  영화 댓글 삭제  */
 export const deleteComment = ({ movieId, userId }: DeleteCommentParams) =>
-  supabase.from('Comments').delete().eq('movie_id', movieId).eq('user_id', userId).then(withSupabaseHandler);
+  withSupabaseHandler(supabase.from('Comments').delete().eq('movie_id', movieId).eq('user_id', userId));
--- src/api/favorite.ts
+++ src/api/favorite.ts
@@ -3,16 +3,16 @@ import { FavoriteData, FavoriteSearchParams } from '@/types/favorite';
 import { withSupabaseHandler } from '@/api/utils';
 
 // 영화 즐겨찾기 조회
-export const fetchFavorites = () => supabase.from('Favorites').select('*').then(withSupabaseHandler);
+export const fetchFavorites = () => withSupabaseHandler(supabase.from('Favorites').select('*'));
 
 // 즐겨찾기 추가
 export const addFavorite = (favoriteData: FavoriteData) =>
-  supabase.from('Favorites').insert([favoriteData]).then(withSupabaseHandler);
+  withSupabaseHandler(supabase.from('Favorites').insert([favoriteData]));
 
 // 즐겨찾기 삭제
 export const deleteFavorite = ({ movieId, userId }: FavoriteSearchParams) =>
-  supabase.from('Favorites').delete().eq('movie_id', movieId).eq('user_id', userId).then(withSupabaseHandler);
+  withSupabaseHandler(supabase.from('Favorites').delete().eq('movie_id', movieId).eq('user_id', userId));
 
 // 특정 영화별 사용자 즐겨찾기에 추가된 리스트 조회
 export const fetchFavoritesByMovie = ({ movieId, userId }: FavoriteSearchParams) =>
-  supabase.from('Favorites').select('*').eq('movie_id', movieId).eq('user_id', userId).then(withSupabaseHandler);
+  withSupabaseHandler(supabase.from('Favorites').select('*').eq('movie_id', movieId).eq('user_id', userId));
