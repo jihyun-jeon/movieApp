d96d565f5f3221058f028dc9985fe34fc94ec674
jihyun-jeon <jhplus13@naver.com>
2025-03-23 00:54:45 +0900

    refac: supabase 응답 처리 방식 통일 : useQuery의 queryFn은 Promise를 반환해야 하는데 supabase는 PromiseLike를 반환하는 문제 : Promise를 반환하도록 타입 강제처리

--- src/api/favorite.ts
+++ src/api/favorite.ts
@@ -1,33 +1,18 @@
 import { supabase } from '@/lib/supabaseClient';
 import { FavoriteData, FavoriteSearchParams } from '@/types/favorite';
+import { withSupabaseHandler } from '@/api/utils';
 
 // 영화 즐겨찾기 조회
-export const fetchFavorites = async () => {
-  const { data, error } = await supabase.from('Favorites').select('*');
-  if (error) throw error;
-  return data;
-};
+export const fetchFavorites = () => withSupabaseHandler(supabase.from('Favorites').select('*'));
 
 // 즐겨찾기 추가
-export const addFavorite = async (favoriteData: FavoriteData) => {
-  const { data, error } = await supabase.from('Favorites').insert([favoriteData]);
-
-  if (error) throw error;
-  return data;
-};
+export const addFavorite = (favoriteData: FavoriteData) =>
+  withSupabaseHandler(supabase.from('Favorites').insert([favoriteData]));
 
 // 즐겨찾기 삭제
-export const deleteFavorite = async ({ movieId, userId }: FavoriteSearchParams) => {
-  const { data, error } = await supabase.from('Favorites').delete().eq('movie_id', movieId).eq('user_id', userId);
-
-  if (error) throw error;
-  return data;
-};
+export const deleteFavorite = ({ movieId, userId }: FavoriteSearchParams) =>
+  withSupabaseHandler(supabase.from('Favorites').delete().eq('movie_id', movieId).eq('user_id', userId));
 
 // 특정 영화별 사용자 즐겨찾기에 추가된 리스트 조회
-export const fetchFavoritesByMovie = async ({ movieId, userId }: FavoriteSearchParams) => {
-  const { data, error } = await supabase.from('Favorites').select('*').eq('movie_id', movieId).eq('user_id', userId);
-
-  if (error) throw error;
-  return data;
-};
+export const fetchFavoritesByMovie = ({ movieId, userId }: FavoriteSearchParams) =>
+  withSupabaseHandler(supabase.from('Favorites').select('*').eq('movie_id', movieId).eq('user_id', userId));
