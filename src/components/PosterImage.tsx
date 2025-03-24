d288ffe9ae9fdac99f4928e3b44a45903ab2f8ad
jihyun-jeon <jhplus13@naver.com>
2025-03-25 03:04:35 +0900

    feat: home화면 에러바운더리 처리 : react-error-boundary 패키지 추가 : Hero, 인기작 캐러셀UI  컴포넌트 분리 : 에러 발생시 대체 UI제공 위한 에러 바운더리 처리

--- src/components/PosterImage.tsx
+++ src/components/PosterImage.tsx
@@ -7,7 +7,7 @@ const PosterImage = ({ posterPath, size }: posterImageType) => (
     {posterPath ? (
       <img src={getImageUrl(posterPath, size)} alt="movie_poster" className="w-full h-full" />
     ) : (
-      <div className="w-full  h-full bg-gray-200"> no poster </div>
+      <div className="absolute inset-0 w-full h-full bg-gray-200"> no poster </div>
     )}
   </>
 );
---
+++ src/pages/Home/components/Day20/index.tsx
@@ -0,0 +1,42 @@
+import { useTrendingMoviesQuery } from '@/hooks/query/useMovie';
+import PosterImage from '@/components/PosterImage';
+import { TMDB_LANGUAGE_KR } from '@/contants';
+import useNavigateToContents from '@/hooks/usePathParams';
+import { Movie } from '@/types/movieType';
+import Carousel from '@/components/Carousel';
+
+export const DayTop20 = () => {
+  const trandingMovies = useTrendingMoviesQuery({ language: TMDB_LANGUAGE_KR, page: 1 });
+  const { updatePathParam } = useNavigateToContents();
+
+  return (
+    <>
+      {trandingMovies.isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <Carousel autoPlay={false} showIndicators={false} showArrows={true} itemsPerPage={5}>
+          {trandingMovies.data?.results.map((movie: Movie) => (
+            <div key={movie.id} className="w-1/5 px-2 cursor-pointer" onClick={() => updatePathParam('.', movie.id)}>
+              <div className="relative">
+                <PosterImage posterPath={movie?.poster_path} size="w342" />
+              </div>
+            </div>
+          ))}
+        </Carousel>
+      )}
+    </>
+  );
+};
+
+//  에러 UI 컴포넌트
+export const DayTop20ErrorFallback = ({ error }: { error: Error }) => (
+  <Carousel autoPlay={false} showIndicators={false} showArrows={true} itemsPerPage={5}>
+    {[...Array(5)].map((_, index) => (
+      <div key={index} className="w-1/5 px-2 cursor-pointer">
+        <div className="relative min-h-[200px]">
+          <PosterImage posterPath={null} size="w342" />
+        </div>
+      </div>
+    ))}
+  </Carousel>
+);
