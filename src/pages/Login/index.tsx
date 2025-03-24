8f499ae2c7b878f5779a8e856574179e2f19814b
Jihyun Jeon <85624671+jihyun-jeon@users.noreply.github.com>
2025-03-24 16:06:23 +0900

    Revert "feat: react-hook-form, zod 를 활용한 로그인,회원가입 폼 구현"
    This reverts commit 586d65d05c159cd6400f9743c1cb2a2eb2982026.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,69 @@
+import { ERROR_MESSAGES } from '@/contants';
 import { useAuth } from '@/context/AuthContext';
-import AuthForm from '@/components/AuthForm';
-import { useNavigate } from 'react-router-dom';
+import { AuthError } from '@supabase/supabase-js';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const { signIn } = useAuth();
   const navigate = useNavigate();
 
-  const handleSignIn = async ({ email, password }: { email: string; password: string }) => {
-    await signIn({ email: email, password: password });
-    navigate('/');
+  const [email, setEmail] = useState(''); // [TODO] form 라이브러리 사용 후 상태 정리
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<null | AuthError>(null);
+
+  const handleSignIn = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      const data = await signIn({ email, password });
+      if (data?.user) {
+        navigate('/');
+        setError(null);
+      }
+    } catch (error) {
+      if (error instanceof AuthError) return setError(error); // supabase 에러
+
+      // 500 서버 에러, 런타임 에러
+      alert(ERROR_MESSAGES.DEFAULT);
+      setError(null);
+    }
   };
 
-  return <AuthForm mode="login" onSubmitFn={handleSignIn} />;
+  return (
+    <div>
+      {/* [TODO] form UI 라이브러리 사용, 유효성 검사 */}
+      <form onSubmit={handleSignIn} className="max-w-md m-auto pt-24">
+        <h2 className="font-bold pb-2">Login</h2>
+        <p>
+          Don&apos;t have an account yet? <Link to="/signup">Sign up</Link>
+        </p>
+        <div className="flex flex-col py-4">
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="p-3 mt-2 text-black"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+        </div>
+        <div className="flex flex-col py-4">
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="p-3 mt-2 text-black"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+        <button className="w-full mt-4">Login</button>
+        {error && <p className="text-red-600 text-center pt-4">아이디 또는 비밀번호를 확인해주세요</p>}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
